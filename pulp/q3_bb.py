3097897da1b01c6cec522e45810e33c22560e007
hoale0231 <leviethoa0231@gmail.com>
2023-04-27 21:04:36 +0700

    nothing

--- pulp/q3_bb.py
+++ pulp/q3_bb.py
@@ -20,21 +20,18 @@ iter = 0
 def brand_and_bound(week_schedule: List[List[int]], days_count_shift: List[Dict[int, int]], lb_shift: List[int], ub_shift: List[int], i = 0, j = 0):
     global iter
     
-    if j == len(week_schedule[0]):
-        raise Success(week_schedule)
-    
     shift_count = [0] * len(SHIFTS)
     for i_ in range(i+1):
         shift_count[week_schedule[i_][j]] += 1
     
     for k in range(len(SHIFTS)):
         if shift_count[k] > ub_shift[k]:
-            iter += 1
-            if iter % 1_000_000 == 0:
-                print('='* 20)
-                print(i, j, k)
-                for day, day_schedule in zip(WEEK, week_schedule):
-                    print(f"{day}, schedule: {day_schedule}")
+            # iter += 1
+            # if iter % 100_000 == 0:
+            #     print('='* 20)
+            #     print(i, j, k)
+            #     for day, day_schedule in zip(WEEK, week_schedule):
+            #         print(f"{day}, schedule: {day_schedule}")
             raise Failed()
         
     if i == len(week_schedule) - 1:
@@ -42,15 +39,19 @@ def brand_and_bound(week_schedule: List[List[int]], days_count_shift: List[Dict[
             if shift_count[k] < lb_shift[k]:
                 raise Failed()
     
-    # j = (j + 1) % len(week_schedule[0])
-    # i = (i + 1) if j == 0 else i
     
     i += 1
     if i == len(week_schedule):
         i = 1
-
     if i == 1:
         j += 1
+    if j == len(week_schedule[0]):
+        raise Success(week_schedule)
+        
+    # j = (j + 1) % len(week_schedule[0])
+    # i = (i + 1) if j == 0 else i
+    # if i == len(week_schedule):
+    #     raise Success(week_schedule)
 
     for k, v in days_count_shift[i].items():
         if v == 0:
@@ -94,16 +95,21 @@ def CSR_schedule(week_requires: List[List[int]]) -> Tuple[int, List[int], List[L
     print(days_count_shift)
     print(lb_shift)
     print(ub_shift)
+    start = time()
     try:
         brand_and_bound(deepcopy(new_week_schedule), days_count_shift, lb_shift, ub_shift, 0, -1)
-        print('cc')
     except Success as e:
-        print(e.week_schedule)
+        # print(e.week_schedule)
         fair_schedule = e.week_schedule
     # except Exception as e:
     #     print(e)
     #     exit()
-        
+    end = time()
+    print(f"Solve time q3 {round(end-start,2)}s")
+    
+    fair_schedule = check_maximum_onboard_day_constraint(fair_schedule)
+    fair_schedule = check_requires_constraint_all_day(fair_schedule, week_requires)
+    fair_schedule = check_fair_scheduling(fair_schedule)
     return fair_schedule
 
 
